8c49439ffc5bd8b65364ed5f6aa6f018204b7e4f
GameForPeople <Koreagamemaker@gmail.com>
2017-10-30 04:34:44 +0900

    Do Backup_008 "Homework1" //please stdafx.h define NEW_CODE //HLSL건드림 ,, 하얀창도 안뜸...ㅎ

--- 3D게임1 과제03(원성연-2013182027)/Executable/Shaders.hlsl
+++ 3D게임1 과제03(원성연-2013182027)/Executable/Shaders.hlsl
@@ -1,14 +1,27 @@
-//게임 객체의 정보를 위한 상수 버퍼를 선언한다.
-cbuffer cbGameObjectInfo : register(b0)
+// 상수버퍼1 은 플레이어정보여
+cbuffer cbPlayerInfo : register(b0)
 {
-    matrix gmtxWorld : packoffset(c0);
+    matrix gmtxPlayerWorld : packoffset(c0);
 };
+
+
 //카메라의 정보를 위한 상수 버퍼를 선언한다.
 cbuffer cbCameraInfo : register(b1)
 {
     matrix gmtxView : packoffset(c0);
     matrix gmtxProjection : packoffset(c4);
+    float3 gvCameraPosition : packoffset(c8);
 };
+
+//게임 객체의 정보를 위한 상수 버퍼를 선언한다.
+cbuffer cbGameObjectInfo : register(b2)
+{
+    matrix gmtxWorld : packoffset(c0);
+    uint gnMaterial : packoffset(c4);
+};
+
+#include "Light.hlsl"
+
 //정점 셰이더의 입력을 위한 구조체를 선언한다.
 struct VS_INPUT
 {
@@ -21,7 +34,103 @@ struct VS_OUTPUT
     float4 position : SV_POSITION;
     float4 color : COLOR;
 };
+
+struct VS_DIFFUSED_INPUT
+{
+    float3 position : POSITION;
+    float4 color : COLOR;
+};
+
+struct VS_DIFFUSED_OUTPUT
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+VS_DIFFUSED_OUTPUT VSGameObject(VS_DIFFUSED_INPUT input)
+{
+    VS_DIFFUSED_OUTPUT output;
+
+    output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxGameObject), gmtxView), gmtxProjection);
+    output.color = input.color;
+
+    return (output);
+}
+
+float4 PSGameObject(VS_DIFFUSED_OUTPUT input) : SV_TARGET
+{
+    return (input.color);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+VS_DIFFUSED_OUTPUT VSPlayer(VS_DIFFUSED_INPUT input)
+{
+    VS_DIFFUSED_OUTPUT output;
+
+    output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxPlayerWorld), gmtxView), gmtxProjection);
+    output.color = input.color;
+
+    return (output);
+}
+
+float4 PSPlayer(VS_DIFFUSED_OUTPUT input) : SV_TARGET
+{
+    return (input.color);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+#define _WITH_VERTEX_LIGHTING
+
+struct VS_LIGHTING_INPUT
+{
+    float3 position : POSITION;
+    float3 normal : NORMAL;
+};
+
+struct VS_LIGHTING_OUTPUT
+{
+    float4 position : SV_POSITION;
+    float3 positionW : POSITION;
+    float3 normalW : NORMAL;
+//	nointerpolation float3 normalW : NORMAL;
+#ifdef _WITH_VERTEX_LIGHTING
+    float4 color : COLOR;
+#endif
+};
+
+VS_LIGHTING_OUTPUT VSLighting(VS_LIGHTING_INPUT input)
+{
+    VS_LIGHTING_OUTPUT output;
+
+    output.normalW = mul(input.normal, (float3x3) gmtxGameObject);
+    output.positionW = (float3) mul(float4(input.position, 1.0f), gmtxGameObject);
+    output.position = mul(mul(float4(output.positionW, 1.0f), gmtxView), gmtxProjection);
+#ifdef _WITH_VERTEX_LIGHTING
+    output.normalW = normalize(output.normalW);
+    output.color = Lighting(output.positionW, output.normalW);
+#endif
+    return (output);
+}
+
+float4 PSLighting(VS_LIGHTING_OUTPUT input) : SV_TARGET
+{
+#ifdef _WITH_VERTEX_LIGHTING
+    return (input.color);
+#else
+	input.normalW = normalize(input.normalW);
+	float4 color = Lighting(input.positionW, input.normalW);
+	return(color);
+#endif
+}
+
+
+
 //정점 셰이더를 정의한다.
+
+/*
 VS_OUTPUT VSDiffused(VS_INPUT input)
 {
     VS_OUTPUT output;
@@ -35,7 +144,7 @@ float4 PSDiffused(VS_OUTPUT input) : SV_TARGET
 {
     return (input.color);
 }
-
+*/
 /*
 //정점 셰이더를 정의한다.
 VS_OUTPUT VSMain(VS_INPUT input)
